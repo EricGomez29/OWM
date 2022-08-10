ce154963789406a7d2a8ea8dc25059df4f752572
egomez472 <erickgoomez472@gmail.com>
2022-08-10 00:28:24 -0300

    Implements ip geolocation with abtractapi

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [ipInfo, setIpInfo] = useState('');
+
+  useEffect(() => {
+    getIpInfo();
+  }, []);
+
+  const getIpInfo = async () => {
+    try {
+      const response = await axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=8c7356483ea445aab475d2ff5543e337');
+      setIpInfo(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Hola Mundo!</h1>
+
+      <div className="header">
+
+      </div>
+      
+      <div className="body">
+        <h1>Hola Mundo!</h1><br/>
+        <span>Localidad: {ipInfo && ipInfo.city ? ipInfo.city : 'No localizado'}</span><br/>
+        <span>Ciudad: {ipInfo && ipInfo.region ? ipInfo.region : 'No localizado'}</span><br/>
+        <span>País: {ipInfo && ipInfo.country ? ipInfo.country : 'No localizado'}</span>
+      </div>
+
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
